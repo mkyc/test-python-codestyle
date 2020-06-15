c51daeb44760a5dce635398ea841022a42cb930a
Mateusz Kyc <mateusz.kyc@gmail.com>
2020-06-15 14:16:03 +0200

    rename test_capitalize add pylint

--- test_capitalize.py
+++ test_capitalize.py
@@ -7,5 +7,5 @@ def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
 
 def test_raises_exception_on_non_string_arguments():
-    with pytest.raises(TypeError):
+    with pytest.raises(AttributeError):
         capital_case(9)
